e5bf1a907b639d3bacdb14a9bdb71bbd961ee1e1
dbertella <daniele.bertella@gmail.com>
2015-03-19 17:58:48 +0100

    ajax navigation working

---
+++ js/appify.js
@@ -0,0 +1,51 @@
+var addListnerToLink = function () {
+	[].forEach.call(document.querySelectorAll('a'), function (el) {
+		el.addEventListener('click', appify);
+	});
+};
+var removeListnerToLink = function () {
+	[].forEach.call(document.querySelectorAll('a'), function (el) {
+		el.removeEventListener('click', appify);
+	});
+};
+
+addListnerToLink();
+
+function appify (e) {
+	e.preventDefault();
+	removeListnerToLink();
+	//this.classList.add("clicked");
+	var ajaxLink = this.href;// getAttribute('href');
+	var xhr = new XMLHttpRequest();  
+	xhr.open("GET", ajaxLink, false);  
+	xhr.send(null);
+	var strVal = xhr.responseText; 
+	var pattern = /<body[^>]*>((.|[\n\r])*)<\/body>/im
+	var newContent = pattern.exec(strVal);
+	var content = document.querySelector('.content');
+	while (content.hasChildNodes()) {
+	    content.removeChild(content.lastChild);
+	}
+
+	var newDiv = document.createElement("div");
+	newDiv.className = "new-content";
+
+	document.body.appendChild(newDiv);
+	
+	newDiv.innerHTML = newContent[0];
+	
+	[].forEach.call(newDiv.querySelectorAll('script'), function (el) {
+		newDiv.removeChild(el);
+	});
+	
+	[].forEach.call(newDiv.querySelectorAll('nav'), function (el) {
+		newDiv.removeChild(el);
+	});
+	
+	content.innerHTML = newDiv.querySelectorAll('.content')[0].innerHTML;
+
+	document.body.removeChild(newDiv);
+	newDiv = null;
+	history.pushState(null, null, ajaxLink);
+	addListnerToLink();
+}
