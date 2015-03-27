96dd06902a01ab8c2ff9cdf4e71fb30ff66e249c
dbertella <daniele.bertella@gmail.com>
2015-03-27 10:01:20 +0100

    trying to get gh-pages working

--- app/js/appify.js
+++ app/js/appify.js
@@ -1,3 +1,14 @@
+// git pages workflow
+// git add .
+// git status // to see what changes are going to be commited
+// git commit -m 'Some descriptive commit message'
+// git push origin master
+
+// git checkout gh-pages // go to the gh-pages branch
+// git rebase master // bring gh-pages up to date with master
+// git push origin gh-pages // commit the changes
+// git checkout master // return to the master branch
+
 var addListnerToLink = function () {
 
 	[].forEach.call(document.querySelectorAll('a'), function (el) {
